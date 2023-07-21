d983312e92bfcc7bf7dfed90fc4231d324c3eda0
aryat <aryatalathi24@gmail.com>
2023-07-21 20:08:36 +0530

    few changes

--- SnakeAndLadderGame.h
+++ SnakeAndLadderGame.h
@@ -3,14 +3,13 @@
 
 class SnakeAndLadderGame {
 private:
-    // Add any private member variables or functions if needed
-
+    
 public:
     void playGame();
     void gamescore(char name1[], char name2[], int p1, int p2);
     void play_dice(int &score);
     void callFunctions();
-    void SLboard(); // Add the declaration of SLboard() function here
+    void SLboard(); 
 };
 
 #endif // SNAKEANDLADDER_H
